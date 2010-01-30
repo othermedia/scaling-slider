afb8006739abffff849024ea16ed4c16635add58
Benedict Eastaugh <benedict@eastaugh.net>
2010-01-30 15:21:43 +0000

    Add a container around the sliding area, and move the area when the handle is dragged.

--- source/scaling-slider.js
+++ source/scaling-slider.js
@@ -5,18 +5,26 @@ ScalingSlider = new JS.Class('ScalingSlider', {
     },
     
     setup: function() {
-        this._handle  = Ojay(Ojay.HTML.div({className: 'slider-handle'}));
-        this._backing = Ojay(Ojay.HTML.div({className: 'slider-backing'}))
-            .insert(this._handle);
+        this._container = Ojay(Ojay.HTML.div({className: 'slider-container'}));
+        this._handle    = Ojay(Ojay.HTML.div({className: 'slider-handle'}));
+        this._backing   = Ojay(Ojay.HTML.div({className: 'slider-backing'})).insert(this._handle);
+        this._slider    = new YAHOO.util.DD(this._handle.node);
         
-        this._slider = new YAHOO.util.DD(this._handle.node);
         this._slider.setYConstraint(0, 0);
-        
-        this._area.insert(this._backing, this._options.position || 'top');
         
         Ojay(window).on('resize', function(win, evnt) {
             this.rescale();
         }, this);
+        
+        this._slider.onDrag = function(evnt) {
+            var offset = Math.floor(this._handle.node.offsetLeft / this._ratio);
+            this._area.animate({left: {to: -offset}}, 0.1);
+        }.bind(this);
+        
+        this._area.insert(this._container, 'before')
+        ._(this._container)
+            .insert(this._area)
+            .insert(this._backing, this._options.position || 'top');
         
         this.rescale();
     },
@@ -24,10 +32,13 @@ ScalingSlider = new JS.Class('ScalingSlider', {
     rescale: function() {
         var portWidth   = Ojay.getViewportSize().width,
             areaWidth   = this._area.getWidth(),
-            ratio       = portWidth / areaWidth,
-            handleWidth = Math.ceil(ratio * portWidth),
-            offsetLeft  = Math.round((handleWidth / this._handle.getWidth()) * this._handle.node.offsetLeft),
-            offsetRight = portWidth - handleWidth - offsetLeft;
+            handleWidth, offsetLeft, offsetRight;
+        
+        this._ratio = portWidth / areaWidth;
+        
+        handleWidth = Math.ceil(this._ratio * portWidth);
+        offsetLeft  = Math.round((handleWidth / this._handle.getWidth()) * this._handle.node.offsetLeft);
+        offsetRight = portWidth - handleWidth - offsetLeft;
         
         this._slider.setXConstraint(offsetLeft, offsetRight);
         
